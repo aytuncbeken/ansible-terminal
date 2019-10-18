7cb710cc56b4653b1b29137ad2ad1f1d240375e7
Name From Git-Plugin-Test <email.from.git.plugin.test@example.com>
2019-10-18 16:53:51 +0200

    setup added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="ansible-ssh",
+      version='1.0',
+      python_requires='>=2.7, <=3.0',
+      description='SSH/SFTP Connection Manager for Ansible',
+      author='Aytunc Beken',
+      author_email='aytuncbeken.ab@gmail.com',
+      packages=find_packages(),
+      install_requires=['ansible'],
+      entry_points={
+          'console_scripts': ['ansible-ssh=cli.main:main']
+      })
