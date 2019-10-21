f37f12f005137d144b3e9c830c147dc8778bd496
Name From Git-Plugin-Test <email.from.git.plugin.test@example.com>
2019-10-21 10:52:14 +0200

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="ansible-ssh",
-      version='0.2.0',
+      version='0.3.0',
       python_requires='>=2.7, <=3.0',
       description='SSH/SFTP Connection Manager for Ansible',
       author='Aytunc Beken',
