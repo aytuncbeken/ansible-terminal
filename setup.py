5444880e60288bf882a45af66dfce02d3dd4499a
Name From Git-Plugin-Test <email.from.git.plugin.test@example.com>
2019-10-21 11:14:28 +0200

    Bump version: 0.0.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name="ansible-ssh",
-      version='0.3.0',
+setup(name="ansible-terminal",
+      version='0.1.0',
       python_requires='>=2.7, <=3.0',
-      description='SSH/SFTP Connection Manager for Ansible',
+      description='SSH/SFTP Terminal Manager for Ansible',
       author='Aytunc Beken',
       author_email='aytuncbeken.ab@gmail.com',
       packages=find_packages(exclude=["bin"]),
       install_requires=['ansible'],
       entry_points={
-          'console_scripts': ['ansible-ssh=cli.main:main']
+          'console_scripts': ['ansible-terminal=cli.main:main']
       })
